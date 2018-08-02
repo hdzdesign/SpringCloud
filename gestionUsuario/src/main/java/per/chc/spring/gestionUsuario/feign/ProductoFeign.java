693bf273fee491696bb70e4efb06207490711e0b
Unknown <chcandroistudio@gmail.com>
2018-08-03 00:09:57 +0200

    cambio de feign absoluta
    todo ha ido bien

--- gestionUsuario/src/main/java/per/chc/spring/gestionUsuario/feign/ProductoFeign.java
+++ gestionUsuario/src/main/java/per/chc/spring/gestionUsuario/feign/ProductoFeign.java
@@ -2,12 +2,13 @@ package per.chc.spring.gestionUsuario.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+@Service
 @FeignClient(name = "ms-gestion-producto")
 public interface ProductoFeign {
-    @RequestMapping(value = "producto/{idUsuario}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/producto/{idUsuario}", method = RequestMethod.DELETE)
     ResponseEntity<String> eliminarProductoByIdUsuario(Long idUsuario);
 }
